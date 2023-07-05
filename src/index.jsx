26357c5aab00b4e2d1a37187ee02f4da0a478240
nikitanosov1 <nikinosov2002@mail.ru>
2023-07-05 23:50:24 +0400

    added concat

--- src/index.jsx
+++ src/index.jsx
@@ -213,7 +213,7 @@ const TABS = {
   },
 };
 for (let i = 0; i < 6; ++i) {
-  TABS.all.items = [...TABS.all.items, ...TABS.all.items];
+  TABS.all.items = [].concat(TABS.all.items, TABS.all.items);
 }
 const TABS_KEYS = Object.keys(TABS);
 
